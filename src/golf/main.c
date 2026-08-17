c26cfb6977d3589bc7b3858272d1f6f5d58c54ee
lorenzograssiUni <lorenzo.grassi@studenti.unicam.it>
2026-08-17 18:03:46 +0200

    Integrate socket listener in main game loop
    - Initialize socket listener on startup.
    - Poll for commands (RESET, NEXT, QUIT) in the game loop.
    - Cleanup socket listener on exit.
    - Re-add socket_listener.c to golf CMakeLists.txt.

--- src/golf/main.c
+++ src/golf/main.c
@@ -6,11 +6,13 @@
 #include "common/inputs.h"
 #include "common/graphics.h"
 #include "common/audio.h"
+#include "socket_listener.h"
 
 #include <sokol/sapp.h>
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 static struct game *g_game;
 static struct inputs *g_inputs;
@@ -20,6 +22,10 @@ static struct audio *g_audio;
 static void
 init_cb(void)
 {
+    if (socket_listener_init() != 0) {
+        log_error("Failed to initialize socket listener");
+    }
+
     g_game = game_create();
     g_inputs = inputs_create();
     g_graphics = graphics_create();
@@ -37,6 +43,20 @@ frame_cb(void)
     draw_frame(g_game, g_graphics, g_inputs);
     ui_frame(g_game, g_graphics, g_inputs);
 
+    char buf[256];
+    int n = socket_listener_poll(buf, sizeof(buf));
+    if (n > 0) {
+        buf[n] = '\0';
+        if (strcmp(buf, "RESET") == 0) {
+            game_init_level(g_game, game_get_current_level(g_game));
+        } else if (strcmp(buf, "NEXT") == 0) {
+            int cur = game_get_current_level(g_game);
+            game_init_level(g_game, cur + 1);
+        } else if (strcmp(buf, "QUIT") == 0) {
+            sapp_request_quit();
+        }
+    }
+
     if (game_should_quit(g_game)) {
         sapp_request_quit();
     }
@@ -45,6 +65,8 @@ frame_cb(void)
 static void
 cleanup_cb(void)
 {
+    socket_listener_cleanup();
+
     game_destroy(g_game);
     inputs_destroy(g_inputs);
     graphics_destroy(g_graphics);
