a41d781281a6b0eac3ba75e2c1134ced931a4ced
lorenzograssiUni <lorenzo.grassi@studenti.unicam.it>
2026-08-17 18:12:12 +0200

    Fix sokol impl.c to not define main() on Windows
    - Wrap SOKOL_IMPL main() definition with #if !defined(_WIN32).
    - Allow golf/main.c to define main() explicitly on Windows.

--- src/3rd_party/sokol/impl.c
+++ src/3rd_party/sokol/impl.c
@@ -1,15 +1,14 @@
-#define SOKOL_WIN32_NO_GL_LOADER
 #define SOKOL_IMPL
-#include "sokol/sokol_audio.h"
-#include "sokol/sokol_gfx.h"
-#include "sokol/sokol_time.h"
+#define SOKOL_GLCORE33
 
-#define SOKOL_WIN32_FORCE_MAIN
-#include "sokol/sokol_app.h"
-#include "sokol/sokol_glue.h"
+#include "sokol/gfx.h"
+#include "sokol/sapp.h"
+#include "sokol/saudio.h"
+#include "sokol/glue.h"
+#include "sokol/imgui.h"
 
-#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
-#include "cimgui/cimgui.h"
-
-#define SOKOL_IMGUI_IMPL
-#include "sokol/sokol_imgui.h"
+#if !defined(_WIN32)
+SOKOL_API_IMPL int main(void) {
+    return sokol_main(0, NULL);
+}
+#endif
